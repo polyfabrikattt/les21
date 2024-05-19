f83483d6f7869cddce7bf770fa958c2d40242c8b
Артуриус <@DESKTOP>
2024-05-19 17:55:20 +0300

    Добавьте файлы проекта.

---
+++ lws21/lws21.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num;
+    cout << "input number ";
+    cin >> num;
+
+    int SmDig ;
+    int SecSmDig ;
+
+    int adj = num;
+    while (adj != 0) {
+        int dig= adj % 10;
+        if (dig < SmDig) {
+            SecSmDig = SmDig;
+            SmDig = dig;
+        }
+        else if (dig < SecSmDig && dig != SmDig) {
+            SecSmDig = dig;
+        }
+        adj /= 10;
+    }
+
+    if (SecSmDig == 0) {
+        cout << "no number" << endl;
+    }
+    else {
+        cout << "You number " << SecSmDig << endl;
+    }
+
+    return 0;
+}
